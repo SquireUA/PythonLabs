7aebf8284ffb4e31eb9f8eec0f0e7ba11ac8f875
SquireUA <valierii.siekrietariuk12@gmail.com>
2023-03-07 10:16:38 +0200

    laba 2

---
+++ Lab2.py
@@ -0,0 +1,31 @@
+#1
+print("Завдання №1\n")
+print("Programing", "Essential", "in", sep="***", end="...")
+print("Python")
+print("\n")
+
+#2
+print("Завдання №2\n")
+
+print("    *  \n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *   *\n  *****\n")
+print("\n")
+#3
+print("Завдання №3\n")
+
+print("I`m student")
+print("\n")
+
+print("Завдання №4\n")
+
+print("\"I`m\"\n\"\"learnig\"\"\n\"\"\"Python\"\"\"")
+print("\n")
+
+#4
+print("Завдання №5\n")
+
+print(0o555)
+print("\n")
+
+print("Завдання №6\n")
+
+print(0x777)
